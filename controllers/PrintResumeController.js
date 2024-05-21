1f306f8d33858fac7a56fda35aef21bde52c9f63
amanglassfrog <aman@glassfrog.design>
2024-05-21 11:34:31 +0530

    puppeteer

--- controllers/PrintResumeController.js
+++ controllers/PrintResumeController.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-const pdf = require('html-pdf-node');
+const puppeteer = require('puppeteer')
 
-const printResumePath = path.join(__dirname, '..', 'resumeTemplate/resume.html');
+const printResumePath = path.join(
+    __dirname,
+    '..',
+    'resumeTemplate/resume.html')
 
 const printResume = async (request, reply) => {
     const htmlbody = request.body.html;
-    const pageContent = fs.readFileSync(printResumePath, 'utf8').toString();
-    const html = pageContent.replace('{{content}}', htmlbody);
-    console.log(html);
-
-    const options = { format: 'A4' };
-    const file = { content: html };
-
+    const page = fs.readFileSync(printResumePath, 'utf8').toString()
+    const html = page.replace('{{content}}', htmlbody);
+    console.log(html)
     try {
-        const pdfBuffer = await pdf.generatePdf(file, options);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setContent(html);
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            printBackground: true,
+        });
+        await browser.close();
         reply.header('Content-Type', 'application/pdf');
         reply.header('Content-Disposition', 'attachment; filename="generated.pdf"');
         reply.send(pdfBuffer);
@@ -22,6 +28,7 @@ const printResume = async (request, reply) => {
         console.error('Error generating PDF:', error);
         reply.status(500).send('Error generating PDF');
     }
-};
 
-module.exports = { printResume };
+}
+
+module.exports = { printResume }
