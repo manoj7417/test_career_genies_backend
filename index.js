07ad9a8a6405084faff1af89e7688e6ba3429fdb
amanglassfrog <aman@glassfrog.design>
2024-07-01 00:43:03 +0530

    update

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const PrintResume = require('./routes/PrintResume');
 const cors = require('@fastify/cors');
 const cookie = require('@fastify/cookie');
 const multer = require('fastify-multer');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.WEBHOOK_ENDPOINTs
 
 require('dotenv').config();
 
