05b7517331d737be4ab8288a3cde0a2065b8b39f
anujrawat@glassfrog.design <anujrawat@glassfrog.design>
2024-11-13 10:46:22 +0530

    (fix) : coach edit new api added

--- controllers/CoachEditController.js
+++ controllers/CoachEditController.js
@@ -29,7 +29,28 @@ const editCoachDetails = async (req, res) => {
     }
 }
 
+const getCoachEditReqById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const editRequest = await CoachEdit.findById(id);
+        if (!editRequest) {
+            return res.status(404).send({
+                status: "FAILURE",
+                message: "Edit request not found"
+            });
+        }
+        res.status(200).send({
+            status: "SUCCESS",
+            editRequest
+        });
+    } catch (error) {
+        console.log("Error", error);
+        res.status(500).send({ status: "FAILURE", message: "An error occurred while getting edit coach details" });
+    }
+}
+
 
 module.exports = {
-    editCoachDetails
+    editCoachDetails,
+    getCoachEditReqById
 }
--- routes/CoachEditRoute.js
+++ routes/CoachEditRoute.js
@@ -1,8 +1,9 @@
-const { editCoachDetails } = require("../controllers/CoachEditController");
+const { editCoachDetails, getCoachEditReqById } = require("../controllers/CoachEditController");
 
 async function CoachEditRoute(fastify, options) {
     fastify.post('/details', { preHandler: fastify.coachAuth }, editCoachDetails)
 
+    fastify.get('/getdetails/:id', { preHandler: fastify.coachAuth }, getCoachEditReqById)
 }
 
 module.exports = CoachEditRoute;
