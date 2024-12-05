0acacf4c94535a6e2bde13b0421f3ee4da3f0de8
amanglassfrog <anujrawat@glassfrog.design>
2024-12-05 11:30:10 +0530

    (fix) : meeting link logic updated

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -4,6 +4,7 @@ const { authorize, createSpace } = require("../utils/googleMeet");
 const { sendEmail } = require("../utils/nodemailer");
 const { OAuth2Client } = require("google-auth-library");
 const { google } = require('googleapis');
+const { v4: uuid } = require('uuid');
 
 const bookSlots = async (req, res) => {
     const user = req.user;
@@ -27,7 +28,7 @@ const bookSlots = async (req, res) => {
             });
         }
 
-        const meetingLink = await createSpace(authorize());
+        let meetingLink
         const newBooking = new Booking({
             userId,
             coachId,
@@ -51,62 +52,73 @@ const bookSlots = async (req, res) => {
         user.bookings.push(newBooking._id);
         await user.save();
 
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_CLIENT_ID,
-            process.env.GOOGLE_CLIENT_SECRET
-        );
-        oauth2Client.setCredentials({
-            refresh_token: coach.googleAuth.refreshToken,
-        });
+        if (coach.googleAuth.refreshToken) {
+            const oauth2Client = new google.auth.OAuth2(
+                process.env.GOOGLE_CLIENT_ID,
+                process.env.GOOGLE_CLIENT_SECRET
+            );
+            oauth2Client.setCredentials({
+                refresh_token: coach.googleAuth.refreshToken,
+            });
 
-        const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+            const calendar = google.calendar({ version: "v3", auth: oauth2Client });
 
-        const convertToISO8601 = (date, time) => {
-            const [hours, minutes] = time
-                .replace(" AM", "")
-                .replace(" PM", "")
-                .split(":");
-            const isPM = time.includes("PM");
-            const hourIn24 = isPM && hours !== "12" ? +hours + 12 : hours === "12" && !isPM ? "00" : hours;
-            return `${date}T${hourIn24.padStart(2, "0")}:${minutes.padStart(2, "0")}:00`;
-        };
-
-        const startDateTime = convertToISO8601(date, slotTime.startTime);
-        const endDateTime = convertToISO8601(date, slotTime.endTime);
-
-        const event = {
-            summary: "Career Coaching Session",
-            description: `Notes: ${notes}\nMeeting Link: ${meetingLink}`,
-            start: {
-                dateTime: startDateTime,
-                timeZone: timezone,
-            },
-            end: {
-                dateTime: endDateTime,
-                timeZone: timezone,
-            },
-            attendees: [
-                { email: user.email },
-                { email: coach.email },
-            ],
-            conferenceData: {
-                entryPoints: [
-                    {
-                        entryPointType: "video",
-                        uri: meetingLink,
-                        label: "Join Meeting",
-                    },
-                ],
-            },
-        };
-        const eventResponse = await calendar.events.insert({
-            calendarId: coach.googleAuth.calendarId || "primary",
-            resource: event,
-            conferenceDataVersion: 1,
-            sendUpdates: "all",
+            const convertToISO8601 = (date, time) => {
+                const [hours, minutes] = time
+                    .replace(" AM", "")
+                    .replace(" PM", "")
+                    .split(":");
+                const isPM = time.includes("PM");
+                const hourIn24 = isPM && hours !== "12" ? +hours + 12 : hours === "12" && !isPM ? "00" : hours;
+                const hourString = hourIn24.toString().padStart(2, "0"); 
+                return `${date}T${hourString}:${minutes.padStart(2, "0")}:00`;
+            };
             
-        });
 
+            const startDateTime = convertToISO8601(date, slotTime.startTime);
+            const endDateTime = convertToISO8601(date, slotTime.endTime);
+
+            const event = {
+                summary: "Career Coaching Session",
+                description: `Notes: ${notes}`,
+                start: {
+                    dateTime: startDateTime,
+                    timeZone: timezone,
+                },
+                end: {
+                    dateTime: endDateTime,
+                    timeZone: timezone,
+                },
+                attendees: [
+                    { email: user.email },
+                    { email: coach.email },
+                ],
+                conferenceData: {
+                    createRequest: {
+                        requestId: uuid(),
+                    }
+                },
+            };
+            const eventResponse = await calendar.events.insert({
+                calendarId: coach.googleAuth.calendarId || "primary",
+                resource: event,
+                conferenceDataVersion: 1,
+                sendUpdates: "all",
+            });
+            meetingLink = eventResponse.data.hangoutLink
+            newBooking.meetingLink = meetingLink
+            await newBooking.save()
+
+            return res.status(201).send({
+                status: "SUCCESS",
+                message: "Slot booked successfully",
+                data: newBooking,
+            });
+        }
+
+        meetingLink = await createSpace(authorize());
+        newBooking.meetingLink = meetingLink;
+        await newBooking.save();
         res.status(201).send({
             status: "SUCCESS",
             message: "Slot booked successfully",
